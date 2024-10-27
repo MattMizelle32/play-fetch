4a8bf6aa336059e9ff11c0392e3b00216f25631c
Matthew Mizelle <MIZELLMJ@dukes.jmu.edu>
2024-10-27 17:14:30 -0400

    Update part2.js

--- js/part2.js
+++ js/part2.js
@@ -1,4 +1,4 @@
-const userList = document.getElementById("user-list");
+onst userList = document.getElementById("user-list");
 
 document.addEventListener("DOMContentLoaded", async () => {
     // This function should GET the first page of users from reqres.in.
@@ -7,4 +7,20 @@ document.addEventListener("DOMContentLoaded", async () => {
     // The format should follow the example user in the HTML file.
 
     // TODO
+    const page = 1;
+    let response = await fetch(`https://reqres.in/api/users?page=${page}`);
+    console.log(response);
+
+    const data = await response.json();
+    console.log("data", data);
+
+    // loop through the data and create a div for each user
+    data.data.forEach(user => {
+        const div = document.createElement("div");
+        div.className = "card";
+        div.innerHTML = `<h2>${user.first_name} ${user.last_name}</h2>
+        <img src="${user.avatar}" alt="${user.first_name} ${user.last_name}"></img>`;
+        userList.appendChild(div);
+    });
 });
+
